0870fef86c1cf2ae78fbbd5c28a7c6b123fc3e35
Andrés Felipe Salinas Martínez <andresali1@hotmail.es>
2024-01-02 11:10:57 -0500

    Finishing project

--- SaleSystemApp/src/app/Components/layout/Pages/dash-board/dash-board.component.ts
+++ SaleSystemApp/src/app/Components/layout/Pages/dash-board/dash-board.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+import { Chart, registerables } from 'chart.js';
+import { DashBoardService } from 'src/app/Services/dash-board.service';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrls: ['./dash-board.component.css']
 })
-export class DashBoardComponent {
+export class DashBoardComponent implements OnInit {
+  incomeTotal: string = '0';
+  salesTotal: string = '0';
+  productsTotal: string = '0';
 
+  constructor(
+    private _dashboardService: DashBoardService
+  ) { }
+
+  /**
+   * Method to show the bar Chart
+   * @param graphicLabel 
+   * @param graphicData 
+   */
+  showGraphics(graphicLabel: any[], graphicData: any[]) {
+    const barChart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: graphicLabel,
+        datasets: [{
+          label: "# de Ventas",
+          data: graphicData,
+          backgroundColor: [
+            'rgba(54, 162, 235, 0.2)'
+          ],
+          borderColor: [
+            'rgba(54, 162, 235, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this._dashboardService.summary().subscribe({
+      next: (data) => {
+        if (data.status) {
+          this.incomeTotal = data.value.incomeTotal;
+          this.salesTotal = data.value.salesTotal;
+          this.productsTotal = data.value.productTotal;
+
+          const dataArray: any[] = data.value.lastWeekSales
+
+          const dateLabelTemp = dataArray.map((value) => value.date);
+          const dataTemp = dataArray.map((value) => value.total);
+
+          this.showGraphics(dateLabelTemp, dataTemp);
+        }
+      },
+      error: (error) => { }
+    });
+  }
 }
--- SaleSystemApp/src/app/Components/layout/layout.component.ts
+++ SaleSystemApp/src/app/Components/layout/layout.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { Menu } from 'src/app/Interfaces/menu';
+
+import { MenuService } from 'src/app/Services/menu.service';
+import { CommonService } from 'src/app/Utilities/common.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  menuList: Menu[] = [];
+  userEmail: string = '';
+  userRole: string = '';
 
+  constructor(
+    private router: Router,
+    private _menuService: MenuService,
+    private _commonService: CommonService
+  ) { }
+
+  ngOnInit(): void {
+    const user = this._commonService.getUserSession();
+
+    console.log(user);
+
+    if (user != null) {
+      this.userEmail = user.email;
+      this.userRole = user.roleDescription;
+
+      this._menuService.list(user.userId).subscribe({
+        next: (data) => {
+          if (data.status) this.menuList = data.value;
+        },
+        error: (error) => { }
+      });
+    }
+  }
+
+  /**
+   * Method to logout an User
+   */
+  logout() {
+    this._commonService.deleteUserSession();
+    this.router.navigate(['login']);
+  }
 }
